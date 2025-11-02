bbcf6cd29a1867af4df08d08c6162bad93a30eec
dvdsvds <kundr5@naver.com>
2025-11-02 17:16:46 +0900

    Logical

--- include/op.hpp
+++ include/op.hpp
@@ -3,17 +3,18 @@
 
 #define BIT 8
 
-namespace arithmetic {
-    constexpr size_t MSB = 0;
-    constexpr size_t LSB = BIT - 1;
-    constexpr std::array<bool, BIT> one = {0,0,0,0,0,0,0,1};
+struct Flags {
+    bool C; // carry
+    bool Z; // zero
+    bool N; // nagative
+    bool V; // overflow
+};
 
-    struct Flags {
-        bool C; // carry
-        bool Z; // zero
-        bool N; // nagative
-        bool V; // overflow
-    };
+constexpr size_t MSB = 0;
+constexpr size_t LSB = BIT - 1;
+
+namespace arithmetics {
+    constexpr std::array<bool, BIT> one = {0,0,0,0,0,0,0,1};
 
     std::pair<std::array<bool, BIT>, Flags> ADD(const std::array<bool, BIT>& a, const std::array<bool, BIT>& b, const bool& is_signed);
     std::pair<std::array<bool, BIT>, Flags> SUB(const std::array<bool, BIT>& a, const std::array<bool, BIT>& b, const bool& is_signed);
@@ -21,5 +22,12 @@ namespace arithmetic {
     std::pair<std::array<bool, BIT>, Flags> DEC(const std::array<bool, BIT>& a, const bool& is_signed);
     std::pair<std::array<bool, BIT>, Flags> NEG(const std::array<bool, BIT>& a, const bool& is_signed);
     Flags CMP(const std::array<bool, BIT>& a, const std::array<bool, BIT>& b, const bool& is_signed);
-    
 };
+
+namespace logical {
+    std::pair<std::array<bool, BIT>, Flags> AND(const std::array<bool, BIT>& a, const std::array<bool, BIT>& b);
+    std::pair<std::array<bool, BIT>, Flags> OR(const std::array<bool, BIT>& a, const std::array<bool, BIT>& b);
+    std::pair<std::array<bool, BIT>, Flags> XOR(const std::array<bool, BIT>& a, const std::array<bool, BIT>& b);
+    std::pair<std::array<bool, BIT>, Flags> NOT(const std::array<bool, BIT>& a);
+    Flags TEST(const std::array<bool, BIT>& a, const std::array<bool, BIT>& b);
+}
--- src/op.cpp
+++ src/op.cpp
@@ -4,121 +4,228 @@
 
 namespace arithmetics {
     std::pair<std::array<bool, BIT>, Flags> ADD(const std::array<bool, BIT>& a, const std::array<bool, BIT>& b, const bool& is_signed) {
-        auto [sum, carry] = adders::RCA(a, b);
+        auto [result, carry] = adders::RCA(a, b);
 
         Flags f{};
         f.C = carry;
         if(is_signed) {
-            f.V = (a[MSB] == b[MSB]) && (sum[MSB] != a[MSB]);
+            f.V = (a[MSB] == b[MSB]) && (result[MSB] != a[MSB]);
         } else {
             f.V = false;
         }
 
-        f.N = sum[MSB];
+        f.N = result[MSB];
         f.Z = true;
-        for(bool bit : sum) {
+        for(bool bit : result) {
             if(bit) {
                 f.Z = false;
                 break;
             }
         }
 
-        return {sum, f};
+        return {result, f};
     }
 
     std::pair<std::array<bool, BIT>, Flags> SUB(const std::array<bool, BIT>& a, const std::array<bool, BIT>& b, const bool& is_signed) {
-        auto [sum, carry] = subtractors::RBS(a, b);
+        auto [result, carry] = subtractors::RBS(a, b);
 
         Flags f{};
         f.C = !carry;
         if(is_signed) {
-            f.V = (a[MSB] != b[MSB]) && (sum[MSB] != a[MSB]);
+            f.V = (a[MSB] != b[MSB]) && (result[MSB] != a[MSB]);
         } else {
             f.V = false;
         }
-        f.N = sum[MSB];
+        f.N = result[MSB];
         f.Z = true;
-        for(bool bit : sum) {
+        for(bool bit : result) {
             if(bit) {
                 f.Z = false;
                 break;
             }
         }
 
-        return {sum, f};
+        return {result, f};
     }
 
     std::pair<std::array<bool, BIT>, Flags> INC(const std::array<bool, BIT>& a, const bool& is_signed) {
-        auto [sum, carry] = adders::RCA(a, one);
+        auto [result, carry] = adders::RCA(a, one);
 
         Flags f{};
         f.C = carry;
-        f.N = sum[MSB];
+        f.N = result[MSB];
         f.Z = true;
-        for(bool bit : sum) {
+        for(bool bit : result) {
             if(bit) {
                 f.Z = false;
                 break;
             }
         }
-        f.V = is_signed && (a[MSB] == 0 && sum[MSB] == 1);
+        f.V = is_signed && (a[MSB] == 0 && result[MSB] == 1);
 
-        return {sum, f};
+        return {result, f};
     }
 
     std::pair<std::array<bool, BIT>, Flags> DEC(const std::array<bool, BIT>& a, const bool& is_signed) {
-        auto [sum, carry] = subtractors::RBS(a, one);
+        auto [result, carry] = subtractors::RBS(a, one);
 
         Flags f{};
         f.C = carry;
-        f.N = sum[MSB];
+        f.N = result[MSB];
         f.Z = true;
-        for(bool bit : sum) {
+        for(bool bit : result) {
             if(bit) {
                 f.Z = false;
                 break;
             }
         }
-        f.V = is_signed && (a[MSB] == 1 && sum[MSB] == 0);
+        f.V = is_signed && (a[MSB] == 1 && result[MSB] == 0);
 
-        return {sum, f};
+        return {result, f};
     }
 
     std::pair<std::array<bool, BIT>, Flags> NEG(const std::array<bool, BIT>& a, const bool& is_signed) {
         std::array<bool, BIT> zero = {0,0,0,0,0,0,0,0};
-        auto [sum, carry] = subtractors::RBS(zero, a);
+        auto [result, carry] = subtractors::RBS(zero, a);
 
         Flags f{};
         f.C = carry;
-        f.N = sum[MSB];
+        f.N = result[MSB];
         f.Z = true;
-        for(bool bit : sum) {
+        for(bool bit : result) {
             if(bit) {
                 f.Z = false;
                 break;
             }
         }
         
-        f.V = std::equal(sum.begin(), sum.end(), std::array<bool, BIT>{1,0,0,0,0,0,0,0}.begin());
+        f.V = std::equal(result.begin(), result.end(), std::array<bool, BIT>{1,0,0,0,0,0,0,0}.begin());
 
-        return {sum, f};
+        return {result, f};
     }
 
     Flags CMP(const std::array<bool, BIT>& a, const std::array<bool, BIT>& b, const bool& is_signed) {
-        auto [sum, carry] = subtractors::RBS(a, b);
+        auto [result, carry] = subtractors::RBS(a, b);
 
         Flags f{};
         f.C = carry;
-        f.N = sum[MSB];
+        f.N = result[MSB];
         f.Z = true;
-        for(bool bit : sum) {
+        for(bool bit : result) {
             if(bit) {
                 f.Z = false;
                 break;
             }
         }
-        f.V = is_signed && (a[MSB] == 1 && sum[MSB] == 0);
+        f.V = is_signed && (a[MSB] == 1 && result[MSB] == 0);
 
+        return f;
+    }
+};
+
+namespace logical {
+    std::pair<std::array<bool, BIT>, Flags> AND(const std::array<bool, BIT>& a, const std::array<bool, BIT>& b) {
+        std::array<bool, 8> result;
+        for(size_t i = MSB; i <= LSB; ++i) {
+            result[i] = gate::AND(a[i], b[i]);
+        }
+
+        Flags f{};
+        f.N = result[MSB];
+        f.Z = true;
+        for(bool bit : result) {
+            if(bit) {
+                f.Z = false;
+                break;
+            }
+        }
+
+        f.C = false;
+        f.V = false;
+        return {result, f};
+    }
+
+    std::pair<std::array<bool, BIT>, Flags> OR(const std::array<bool, BIT>& a, const std::array<bool, BIT>& b) {
+        std::array<bool, 8> result;
+        for(size_t i = MSB; i <= LSB; ++i) {
+            result[i] = gate::OR(a[i], b[i]);
+        }
+
+        Flags f{};
+        f.N = result[MSB];
+        f.Z = true;
+        for(bool bit : result) {
+            if(bit) {
+                f.Z = false;
+                break;
+            }
+        }
+
+        f.C = false;
+        f.V = false;
+        return {result, f};
+    }
+
+    std::pair<std::array<bool, BIT>, Flags> XOR(const std::array<bool, BIT>& a, const std::array<bool, BIT>& b) {
+        std::array<bool, 8> result;
+        for(size_t i = MSB; i <= LSB; ++i) {
+            result[i] = gate::XOR(a[i], b[i]);
+        }
+
+        Flags f{};
+        f.N = result[MSB];
+        f.Z = true;
+        for(bool bit : result) {
+            if(bit) {
+                f.Z = false;
+                break;
+            }
+        }
+
+        f.C = false;
+        f.V = false;
+        return {result, f};
+    }
+
+    std::pair<std::array<bool, BIT>, Flags> NOT(const std::array<bool, BIT>& a) {
+        std::array<bool, 8> result;
+        for(size_t i = MSB; i <= LSB; ++i) {
+            result[i] = gate::NOT(a[i]);
+        }
+
+        Flags f{};
+        f.N = result[MSB];
+        f.Z = true;
+        for(bool bit : result) {
+            if(bit) {
+                f.Z = false;
+                break;
+            }
+        }
+
+        f.C = false;
+        f.V = false;
+        return {result, f};
+    }
+
+    Flags TEST(const std::array<bool, BIT>& a, const std::array<bool, BIT>& b) {
+        std::array<bool, 8> result;
+        for(size_t i = MSB; i <= LSB; ++i) {
+            result[i] = gate::AND(a[i], b[i]);
+        }
+
+        Flags f{};
+        f.N = result[MSB];
+        f.Z = true;
+        for(bool bit : result) {
+            if(bit) {
+                f.Z = false;
+                break;
+            }
+        }
+
+        f.C = false;
+        f.V = false;
         return f;
     }
 }
